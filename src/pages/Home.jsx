03e7d80d82b5619b6669cd0eb847a575edade3da
mim-mel <mfblade2014@gmail.com>
2022-09-01 17:02:40 +0900

    feat : mockdata를 통해 화면 요소 구성하기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,46 +2,45 @@ import Navigation from "../components/Navigation";
 import ProductCard from "../components/ProductCard";
 import ThemaButton from "../components/ThemaButton";
 import styled from "styled-components";
+import { useContext, useEffect, useState } from "react";
+import { MockDataContext } from "../App";
 
 const Home = () => {
+  const data = useContext(MockDataContext);
+
+  console.log(data);
+
+  const [themaType, setThemaType] = useState("cup");
+
   return (
     <div>
       <NavigationSection>
         <Navigation />
       </NavigationSection>
       <ThemaSection>
-        <ThemaButton themaName={"#겨울 방한템"} />
-        <ThemaButton themaName={"#따순 머그컵"} />
+        <ThemaButton
+          themaName={"#겨울 방한템"}
+          onClick={() => {
+            setThemaType("cup");
+          }}
+        />
+        <ThemaButton
+          themaName={"#따순 머그컵"}
+          onClick={() => {
+            setThemaType("winter");
+          }}
+        />
       </ThemaSection>
       <GrayLine />
       <ProductSection>
-        <ProductCard
-          name={"비숑 블랙 머그잔"}
-          description={
-            "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다."
-          }
-          thumbnail={
-            "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg"
-          }
-        />
-        <ProductCard
-          name={"가열 보온 티코스터 온열 원터치 컵 받침대"}
-          description={
-            "언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다."
-          }
-          thumbnail={
-            "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
-          }
-        />
-        <ProductCard
-          name={"벨루즈까사 솜사탕 파스텔 머그 4종 세트"}
-          description={
-            "솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그"
-          }
-          thumbnail={
-            "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg"
-          }
-        />
+        {data.map((it) => (
+          <ProductCard
+            key={it.id}
+            name={it.name}
+            description={it.description}
+            thumbnail={it.thumbnail}
+          />
+        ))}
       </ProductSection>
     </div>
   );
