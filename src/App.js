7f03cc30a40b562787a6fd09b6f704558825c5a9
mim-mel <mfblade2014@gmail.com>
2022-09-01 16:53:58 +0900

    feat : context로 mockdata파일 전역관리하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 
 import "./App.css";
-import Navigation from "./components/Navigation";
+import mockData from "./data/mockData";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/Detailpage/ProductDetail";
 import Basket from "./pages/Basket";
+import { createContext } from "react";
+
+export const MockDataContext = React.createContext();
 
 function App() {
   return (
-    <BrowserRouter basename="comento-shop">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:productId" element={<ProductDetail />} />
-        <Route path="/basket" element={<Basket />} />
-      </Routes>
-    </BrowserRouter>
+    <MockDataContext.Provider value={mockData}>
+      <BrowserRouter basename="comento-shop">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="/basket" element={<Basket />} />
+        </Routes>
+      </BrowserRouter>
+    </MockDataContext.Provider>
   );
 }
 
