442465c48cf72751017f255488ac001ee8762457
mim-mel <mfblade2014@gmail.com>
2022-09-07 15:27:46 +0900

    feat : 장바구니 아이템들 배송비를 포함한 총 금액 구하기

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -8,23 +8,60 @@ import { getFromStorage, removeItem } from "../utils/storage";
 const Basket = () => {
   const navigate = useNavigate();
   const [cartItem, setCartItems] = useState([]);
-  const [basketLength, setBasketLength] = useState(0);
+  const [basketItemLength, setBasketItemLength] = useState(0);
+  const [addPrice, setAddPrice] = useState(0);
+  const [delivery, setDelivery] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     const items = getFromStorage();
     setCartItems(items);
-    setBasketLength(items.length);
+    setBasketItemLength(items.length);
+
+    //상품 총합 구하기
+    const price = items.map((it) => it.price);
+    const totalPrice = price.reduce((a, b) => a + b);
+    setAddPrice(totalPrice);
+
+    //배송비 변경
+    let deliveryPrice;
+    if (totalPrice >= 20000) {
+      deliveryPrice = 0;
+    } else {
+      deliveryPrice = 2500;
+    }
+    setDelivery(deliveryPrice);
+
+    //총합구하기
+    const endPrice = totalPrice + deliveryPrice;
+    setTotalPrice(endPrice);
   }, []);
 
   //무한루프를 방지하기 위해서 useEffect를 두번 만들어준다
   useEffect(() => {
     const items = getFromStorage();
     setCartItems(items);
-  }, [basketLength]);
+
+    //상품 총합 구하기
+    const price = items.map((it) => it.price);
+    const totalPrice = price.reduce((a, b) => a + b);
+    setAddPrice(totalPrice);
+
+    //배송비 변경
+    let deliveryPrice;
+
+    if (totalPrice >= 20000) {
+      deliveryPrice = 0;
+    } else {
+      deliveryPrice = 2500;
+    }
+
+    setDelivery(deliveryPrice);
+  }, [basketItemLength]);
 
   const onRemove = (productId) => {
     removeItem(productId);
-    setBasketLength(basketLength - 1);
+    setBasketItemLength(basketItemLength - 1);
   };
 
   return (
@@ -46,7 +83,18 @@ const Basket = () => {
             </TextBlock>
           </CartItemBox>
         ))}
-        <div>상품금액({basketLength})개</div>
+        <TextBox>
+          <div>상품 ({basketItemLength})개</div>
+          <div>{addPrice}원</div>
+        </TextBox>
+        <TextBox>
+          <div>배송비 (2만원 이상 무료배송)</div>
+          <div>{delivery}원</div>
+        </TextBox>
+        <TextBox>
+          <div>총 비용</div>
+          <div>{totalPrice}원</div>
+        </TextBox>
         <BottomButton text={"주문하기"} />
       </Wrap>
     </>
@@ -106,4 +154,12 @@ const XButton = styled.div`
   color: #9e9e9e;
   cursor: pointer;
 `;
+
+const TextBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 370px;
+  margin: 10px 0;
+`;
+
 export default Basket;
